721e990bde2505f22e37bd11bb494d1cfba9ce03
ffahri <fahri.yardimci@gazi.edu.tr>
2016-02-04 12:42:30 +0200

    Added c file and readme

---
+++ easylogs.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+void ufwlog(void);
+void authlog(void);
+void maillog(void);
+void freemem(void);
+void a(void);
+int main()
+{
+	void (*f[5])(void) = {a,ufwlog,authlog,maillog,freemem};
+	int choose=0;
+	while(choose!=-1)
+	{
+
+		printf("\e[1;31m1.Show last 10 lines on ufw.log (/var/log/ufw.log)\n\e[1;32m2.Show last 10 lines on auth.log(/var/log/auth.log)\n");
+		printf("\e[1;36m3.Show last 10 lines on mail.log (/var/log/mail.log)\n\e[1;37m4.Free memory (/proc/sys/vm/drop_caches)*YOU HAVE TO OPEN WITH SUDO*\n");
+		printf("\e[0;0mChoose and write number. For exit : -1\n");
+		scanf("%d",&choose);
+		if(choose>-1 && choose<5)
+		(*f[choose])();
+
+	}
+}
+void ufwlog(void)
+{	
+	system("tail /var/log/ufw.log");
+}
+void authlog(void)
+{
+	system("tail /var/log/auth.log");
+}
+void maillog(void)
+{
+	system("tail /var/log/mail.log");
+}
+void freemem(void)
+{
+	printf("Current use\n");
+	system("free -h");
+	system("echo 3 > /proc/sys/vm/drop_caches");
+	system("free -h");
+}
+void a(void){}
